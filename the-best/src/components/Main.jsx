124299f431e14938aca496ffe3ac3aee2551b31e
Lucas Silva de Jesus <82274693+jesuslucass@users.noreply.github.com>
2021-09-29 15:36:17 -0300

    Update Main.jsx

--- the-best/src/components/Main.jsx
+++ the-best/src/components/Main.jsx
@@ -37,7 +37,7 @@ function Main(){
             return
         }}
     async function GetMovie(movie){
-      const api_key = 'd18a4f16ec6506238fafbda0ee9d740d'
+      const api_key = ''
       const response= await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${movie}&language=pt-br`)
       const data = await response.json()
       const {results} = data
